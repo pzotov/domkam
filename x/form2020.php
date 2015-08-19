d157e8b5bcfc3c780282cbc7306aaf28b4be60e0
Pavel Zotov <pavel.v.zotov@gmail.com>
2015-08-19 22:14:24 +0500

    today

--- x/form2020.php
+++ x/form2020.php
@@ -8,6 +8,17 @@
 
 <div class='nc-field'><?= nc_text_field('Text', "", ($class_id ? $class_id : $classID), 1) ?></div>
 
+<h3>Географические координаты для карты проектов</h3>
+
+<div class="nc-field">
+	<span class="nc-field-caption">Широта</span>
+	<?= nc_float_field('Lat', 'size="12"', ($class_id ? $class_id : $classID)) ?>
+</div>
+<div class="nc-field">
+	<span class="nc-field-caption">Долгота</span>
+	<?= nc_float_field('Lon', 'size="12"', ($class_id ? $class_id : $classID)) ?>
+</div>
+
 <h3>Использованные материалы</h3>
 <?
 if(isset($_POST['stones'])) $stones = $_POST['stones'];
